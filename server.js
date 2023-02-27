8b29218a3760d17ea63d01198ea3b3858700c236
Fifinella <h.hoesley@gmail.com>
2023-02-27 10:45:25 -0800

    added html.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const path = require("path");
-const api = require("./routes/api.js");
+
+// const api = require("./routes/api.js");
 const html = require("./routes/html.js");
 
 const app = express();
@@ -9,7 +9,7 @@ const PORT = 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use("/api", api);
+// app.use("/api", api);
 app.use("/", html);
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
